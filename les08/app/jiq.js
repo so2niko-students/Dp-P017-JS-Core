d9b1c5bdd3caaed715b15fab852fd61f78689ff0
Nick Sotula <so2niko@gmail.com>
2019-11-15 12:05:52 +0200

    classes, map, filter, foreach

---
+++ les08/app/jiq.js
@@ -0,0 +1,6 @@
+const $ = selo => document.querySelector(selo);
+const $$ = selo => document.querySelectorAll(selo);
+const btn = document.querySelector('.click');
+const btn2 = $('.click');
+console.log(btn2);
+console.log($$('p'));
